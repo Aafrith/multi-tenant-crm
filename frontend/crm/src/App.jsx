2859dcaaa4b6d0e3b6ea1ac8cecf1fb58952192e
Aafrith <maafrith15919@gmail.com>
2026-03-02 23:54:09 +0530

    Protected routing and routing added

---
+++ frontend/crm/src/App.jsx
@@ -0,0 +1,41 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Companies from "./pages/Companies";
+import CompanyDetail from "./pages/CompanyDetail";
+import ActivityLogs from "./pages/ActivityLogs";
+
+import PrivateRoute from "./routes/PrivateRoute";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/" element={
+        <PrivateRoute>
+          <Dashboard />
+        </PrivateRoute>
+      } />
+
+      <Route path="/companies" element={
+        <PrivateRoute>
+          <Companies />
+        </PrivateRoute>
+      } />
+
+      <Route path="/companies/:id" element={
+        <PrivateRoute>
+          <CompanyDetail />
+        </PrivateRoute>
+      } />
+
+      <Route path="/logs" element={
+        <PrivateRoute>
+          <ActivityLogs />
+        </PrivateRoute>
+      } />
+    </Routes>
+  );
+}
